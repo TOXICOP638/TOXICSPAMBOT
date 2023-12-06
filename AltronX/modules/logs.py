f138564326cd179e6c9bdc5861a0aacbedbc8f5f
TOXICOP638 <149950962+TOXICOP638@users.noreply.github.com>
2023-12-06 11:35:42 +0530

    logs.py

--- AltronX/modules/logs.py
+++ AltronX/modules/logs.py
@@ -38,9 +38,9 @@ async def logs(legend):
         ms = (end-start).seconds
         await asyncio.sleep(1)
         await fetch.delete()
-        logfile = open("godfather.txt", "w")
-        logfile.write("⚡ GodfatherX ⚡ [ BotSpam Logs ]\n\n" + logs)
+        logfile = open("toxic.txt", "w")
+        logfile.write("☢️ Toxic ☢️ [ BotSpam Logs ]\n\n" + logs)
         logfile.close()
-        await MK1.send_file(legend.chat_id, "GodfatherLogs.txt", caption=f"⚡ 𝙶𝙾𝙳𝙵𝙰𝚃𝙷𝙴𝚁 ⚡\n  » **ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:** `{ms} ꜱᴇᴄᴏɴᴅꜱ`")
+        await MK1.send_file(legend.chat_id, "toxicLogs.txt", caption=f"⚡ TOXIC ⚡\n  » **ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:** `{ms} ꜱᴇᴄᴏɴᴅꜱ`")
     else:
         await legend.reply("» ꜱᴏʀʀʏ, ᴏɴʟʏ ᴏᴡɴᴇʀ ᴄᴀɴ ᴀᴄᴄᴇꜱꜱ ᴛʜɪꜱ ᴄᴏᴍᴍᴀɴᴅ.")
