7538c2b228c57a82d258fb0900a25e9c98355d68
TOXICOP638 <149950962+TOXICOP638@users.noreply.github.com>
2023-12-06 11:30:08 +0530

    start.py

--- AltronX/modules/start.py
+++ AltronX/modules/start.py
@@ -8,11 +8,11 @@ PythonButton = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
         ],
         [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/THE_INCRICIBLE"),
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/INCRICIBLE")
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/HEROKU_CC_STORE"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/DOSTO_KI_M3HFIL")
         ],
         [
-        Button.url("• ʀᴇᴘᴏ •", "https://t.me/The_Incricible/24")
+        Button.url("• ʀᴇᴘᴏ •", "https://graph.org/file/5f1eda2873f74bad0c367.jpg")
         ]
         ]
 
@@ -34,11 +34,11 @@ async def start(event):
         BotName = AltBot.first_name
         BotId = AltBot.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{BotName}](tg://user?id={BotId})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [ʟᴇɢᴇɴᴅ](https://t.me/YOUR_GODFATHER_XD)**\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [ʟᴇɢᴇɴᴅ](https://t.me/NOOBS_ALWAYS_OP)**\n\n"
         TEXT += f"» **ʙᴏᴛ ꜱᴘᴀᴍ ᴠᴇʀsɪᴏɴ :** `M3.2`\n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{telethon.__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                 event.chat_id,
-                "https://telegra.ph/file/71140aa351def22606450.jpg",
+                "https://graph.org/file/5f1eda2873f74bad0c367.jpg",
                 caption=TEXT, 
                 buttons=PythonButton)
